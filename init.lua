a5ee3f5d2723128a3973a97903c2dd9312a2a815
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-03-31 11:42:45 +0000

    set new LSPs

--- init.lua
+++ init.lua
@@ -539,12 +539,14 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         astro = {},
+        cssls = {},
+        jsonls = {},
+        typos_lsp = {},
         graphql = {},
         yamlls = {},
         ansiblels = {},
         bashls = {},
         denols = {},
-        -- clangd = {},
         gopls = {},
         pyright = {},
         terraformls = {},
