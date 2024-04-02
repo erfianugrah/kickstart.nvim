28fb1ad543b94488a1daa194e348218340d50cc9
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-04-02 07:10:11 +0000

    treesitter highlighting

--- init.lua
+++ init.lua
@@ -805,7 +805,27 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'mermaid',
+        'vim',
+        'vimdoc',
+        'astro',
+        'css',
+        'csv',
+        'javascript',
+        'json',
+        'terraform',
+        'sql',
+        'typescript',
+        'yaml',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
