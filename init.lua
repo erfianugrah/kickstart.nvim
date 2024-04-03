0c8e4fd24fec5cd551e86b96ef6922b0bc9e3497
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-04-03 08:39:15 +0000

    reinstated markdown stuff

--- init.lua
+++ init.lua
@@ -554,6 +554,7 @@ require('lazy').setup({
         tailwindcss = {},
         docker_compose_language_service = {},
         dockerls = {},
+        marksman = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -822,6 +823,7 @@ require('lazy').setup({
         'lua',
         'luadoc',
         'markdown',
+        'markdown_inline',
         'mermaid',
         'vim',
         'vimdoc',
