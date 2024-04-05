464799a677dba80b030c4ca8ccec427cf57abaa3
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-04-05 08:04:16 +0000

    add pencil and markdown preview

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -47,4 +47,18 @@ return {
       }
     end,
   },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown', 'mdx' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
+  {
+    'preservim/vim-pencil',
+    init = function()
+      vim.g['pencil#wrapModeDefault'] = 'soft'
+    end,
+  },
 }
