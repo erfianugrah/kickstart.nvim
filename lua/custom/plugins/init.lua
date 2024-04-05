9e9158e208bc5ac80c989d9e7613f3a770ee1b94
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-04-05 08:07:33 +0000

    removed mdx

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -50,7 +50,7 @@ return {
   {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    ft = { 'markdown', 'mdx' },
+    ft = { 'markdown' },
     build = function()
       vim.fn['mkdp#util#install']()
     end,
