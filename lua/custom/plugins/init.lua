69d06cff171a7a40fe55e269e847e3dcb9cb61c1
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-04-07 12:42:44 +0000

    added self-hosted llama and auth

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -80,9 +80,9 @@ return {
   {
     'David-Kunz/gen.nvim',
     opts = {
-      model = 'dolphin-mixtral', -- The default model to use.
-      host = 'localhost', -- The host running the Ollama service.
-      port = '11434', -- The port on which the Ollama service is listening.
+      model = 'dolphin-mistral', -- The default model to use.
+      host = 'ollama.erfianugrah.com', -- The host running the Ollama service.
+      port = '443', -- The port on which the Ollama service is listening.
       quit_map = 'q', -- set keymap for close the response window
       retry_map = '<c-r>', -- set keymap to re-send the current prompt
       init = function(options)
@@ -90,19 +90,35 @@ return {
       end,
       -- Function to initialize Ollama
       command = function(options)
+        local client_id = os.getenv 'CLOUDFLARE_ACCESS_OLLAMA_ID'
+        local client_secret = os.getenv 'CLOUDFLARE_ACCESS_OLLAMA_SECRET'
         local body = { model = options.model, stream = true }
-        return 'curl --silent --no-buffer -X POST http://' .. options.host .. ':' .. options.port .. '/api/chat -d $body'
+        local headers = {
+          ['CF-Access-Client-Id'] = client_id,
+          ['CF-Access-Client-Secret'] = client_secret,
+        }
+        return 'curl --silent --no-buffer -X POST https://'
+          .. options.host
+          .. ':'
+          .. options.port
+          .. '/api/chat -d $body'
+          .. ' -H "CF-Access-Client-Id: '
+          .. client_id
+          .. '"'
+          .. ' -H "CF-Access-Client-Secret: '
+          .. client_secret
+          .. '"'
       end,
       -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
       -- This can also be a command string.
       -- The executed command must return a JSON object with { response, context }
       -- (context property is optional).
       -- list_models = '<omitted lua function>', -- Retrieves a list of model names
-      display_mode = 'float', -- The display mode. Can be "float" or "split".
-      show_prompt = false, -- Shows the prompt submitted to Ollama.
-      show_model = false, -- Displays which model you are using at the beginning of your chat session.
+      display_mode = 'split', -- The display mode. Can be "float" or "split".
+      show_prompt = true, -- Shows the prompt submitted to Ollama.
+      show_model = true, -- Displays which model you are using at the beginning of your chat session.
       no_auto_close = false, -- Never closes the window automatically.
-      debug = false, -- Prints errors and the command which is run.
+      debug = true, -- Prints errors and the command which is run.
     },
   },
 }
