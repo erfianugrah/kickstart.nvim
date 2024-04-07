84fe220e679f35d90a70072ef4328caa7d8d4bf9
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-04-07 16:21:32 +0000

    add mermaid and rust analyzer

--- init.lua
+++ init.lua
@@ -549,7 +549,6 @@ require('lazy').setup({
         typos_lsp = {},
         graphql = {},
         yamlls = {},
-        ansiblels = {},
         bashls = {},
         denols = {},
         gopls = {},
@@ -567,8 +566,8 @@ require('lazy').setup({
         grammarly = {},
         jqls = {},
         vale_ls = {},
-
-        -- rust_analyzer = {},
+        mermaidjs = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
