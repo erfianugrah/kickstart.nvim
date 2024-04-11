ed0fb887b08934cf68fb9acadd8eb41460af441c
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-04-11 06:17:05 +0000

    ltex, marksman, markdown_oxide

--- init.lua
+++ init.lua
@@ -567,6 +567,9 @@ require('lazy').setup({
         jqls = {},
         vale_ls = {},
         rust_analyzer = {},
+        ltex = {},
+        markdown_oxide = {},
+        marksman = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
