30c1e597ebfbbdb0a8827b9d6213bf64a8d3e722
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-05-01 14:57:26 +0000

    grammarly and marksman

--- init.lua
+++ init.lua
@@ -603,6 +603,9 @@ require('lazy').setup({
         vale_ls = {},
         rust_analyzer = {},
         ltex = {},
+        marksman = {},
+        grammarly = {},
+
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
