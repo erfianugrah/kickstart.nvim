63e901daeaf488dd95ab5a0c8abf6bdbd4f53f8f
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-05-01 08:35:47 +0000

    tpope vim markdown vim color pencil

--- init.lua
+++ init.lua
@@ -242,6 +242,8 @@ require('lazy').setup({
   'tpope/vim-fugitive',
   'ms-jpq/coq_nvim',
   'mbbill/undotree',
+  'tpope/vim-markdown',
+  'preservim/vim-colors-pencil',
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
