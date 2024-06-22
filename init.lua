001cd6eaa29b4049b183ba6957919a75eb870bb9
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-06-22 07:56:28 +0000

    prettier

--- init.lua
+++ init.lua
@@ -705,7 +705,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        astro = { 'prettier', 'prettier_d', 'eslint_d' },
+        -- astro = { 'prettier', 'prettier_d', 'eslint_d' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
